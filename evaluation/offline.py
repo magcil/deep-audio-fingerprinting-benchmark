a761b6ff35ecf52e08ffedbdac120c641f5dffe3
ChrisNick92 <chrisnick92@gmail.com>
2024-12-09 13:13:38 +0200

    Offline evaluation refinement

--- evaluation/offline.py
+++ evaluation/offline.py
@@ -4,6 +4,8 @@ import sys
 import json
 import time
 import logging
+from itertools import product
+import datetime
 
 project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, project_path)
@@ -14,8 +16,10 @@ from audiomentations import AddBackgroundNoise
 from tqdm import tqdm
 import librosa
 import numpy as np
+from prettytable import PrettyTable
+import yaml
 
-from models.neural_fingerprinter import Neural_Fingerprinter
+from utils.torch_utils import get_model
 from utils.utils import crawl_directory, extract_mel_spectrogram, query_sequence_search, search_index
 from utils.metrics import summary_metrics
 
@@ -23,7 +27,7 @@ from utils.metrics import summary_metrics
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Evaluation of a DNN on a list of songs for given Query lengths and SNRs.')
-    parser.add_argument('-i', '--config', required=True, help='The evaluation configuration file.')
+    parser.add_argument('-c', '--config', required=True, help='The evaluation configuration file.')
 
     return parser.parse_args()
 
@@ -33,55 +37,87 @@ if __name__ == '__main__':
     args = parse_args()
     config_file = os.path.join(project_path, args.config)
     with open(config_file, 'r') as f:
-        args = json.load(f)
+        args = yaml.safe_load(f)
 
     # Initialize logger
-    logging.basicConfig(filename=os.path.join(project_path, 'logs', args['experiment_name'] + '_offline_test.log'),
+    date = datetime.datetime.now().date()
+    logging.basicConfig(filename=os.path.join(project_path, 'logs',
+                                              args['experiment_name'] + f'_offline_test_{date}.log'),
                         encoding='utf-8',
                         level=logging.INFO,
                         force=True,
                         filemode='w',
                         format='%(asctime)s %(message)s')
-    logging.info(f'Config:\n{args}\n')
 
-    device = 'cuda' if torch.cuda.is_available() and args['device'] == 'cuda' else 'cpu'
-    model = Neural_Fingerprinter().to(device)
-    model.load_state_dict(torch.load(os.path.join(project_path, args['model']), map_location=device, weights_only=True))
+    logging.info(f"{5*'*'}Config{5*'*'}:\n")
+    for k, v in args.items():
+        logging.info(f'{k}: {v}')
+
+    # Set device
+    device = args.get("device", "cpu")
     logging.info(f'Running on {device}')
 
-    index = faiss.read_index(os.path.join(project_path, args['index']))
-    with open(os.path.join(project_path, args['json'])) as f:
-        json_correspondence = json.load(f)
+    # Initialize Result Table
+    result_table = PrettyTable()
+    result_table.field_names = ['Architecture', 'Query length [s] / SNR [dB]', *args['snrs']]
 
-    sorted_arr = np.sort(np.array(list(map(int, json_correspondence.keys()))))
+    # Get Query / SNRs / Models
+    query_lengths = args['query_lengths']
     snrs = args['snrs']
-    durations = args['durations']
-    F = args['sr']
-    H = args['hop_size']
-    k = args['neighbors']
-    index.nprobes = args['nprobes']
+    models = args['models']
 
-    logging.info(f'\nConfigurations:\nModel: {os.path.basename(args["model"])}\nFaiss vectors: {index.ntotal}\n')
+    # Get songs for test / background noises
+    test_songs = crawl_directory(os.path.join(project_path, args['test_songs']), extension='wav')
+    background_noises = os.path.join(project_path, args['background_noises'])
 
-    demo_songs = crawl_directory(os.path.join(project_path, args['demo_songs']), extension='wav')
-    background_noises = os.path.join(project_path, args['background_noise'])
-    dur_times = {}
+    # Set sampling rate / hop length
+    F, H = args['sr'], args['hop_size']
 
-    for dur in durations:
-        logging.info(f'{10 * "*"} Deep audio test with Query duration: {dur} {10 * "*"}\n')
-        top_1_hit_rates = []
-        query_times, inference_times = [], []
+    # Iterate over all combinations (Query, SNR)
+    for iter, (query_length, snr) in enumerate(product(query_lengths, snrs), start=1):
+        # Set Noise augmentation
+        b_noise = AddBackgroundNoise(sounds_path=background_noises, min_snr_in_db=snr, max_snr_in_db=snr, p=1.)
 
-        for snr in snrs:
-            logging.info(f'\n{10 * "*"} SNR: {snr} {10 * "*"}\n')
-            b_noise = AddBackgroundNoise(sounds_path=background_noises, min_snr_in_db=snr, max_snr_in_db=snr, p=1.)
+        # Model rows
+        model_rows = {model_num: [query_length] for model_num in models.keys()}
+
+        # Iterate over all models
+        for model_num, model_info in models.items():
+            # Initialize model
+            model = get_model(model_str=model_info['architecture']).to(device)
+            # Load weights
+            model.load_state_dict(
+                torch.load(os.path.join(project_path, model_info['weights']), map_location=device, weights_only=True))
+            # Get Faiss and Json
+            index = faiss.read_index(os.path.join(project_path, model_info['index']))
+            with open(os.path.join(project_path, model_info['json'])) as f:
+                json_correspondence = json.load(f)
+                sorted_arr = np.sort(np.array(list(map(int, json_correspondence.keys()))))
+
+            index.nprobes = model_info['nprobes']
+            neighbors = model_info['neighbors']
+
+            # Set model name
+            model_name = model_info['architecture'] + " (filters)" if model_info[
+                'filters'] else model_info['architecture'] + " (no filters)"
+
+            # Log config
+            logging.info(
+                f"Offline test of model {model_name} | Query: {query_length} | SNR: {snr} | Vectors: {index.ntotal}")
+
+            # Hold Statistics
             y_true, y_pred = [], []
             offset_true, offset_pred = [], []
+            query_times, inference_times = [], []
 
-            for audio_file in tqdm(demo_songs):
+            for audio_file in tqdm(test_songs):
 
+                # Read Audio
                 y, sr = librosa.load(audio_file, sr=F)
-                segs = (y.size // F) // dur
+
+                # Calculate num segs based on query length
+                segs = (y.size // F) // query_length
+
                 model.eval()
 
                 with torch.no_grad():
@@ -89,7 +125,7 @@ if __name__ == '__main__':
 
                         # Inference
                         tic = time.perf_counter()
-                        y_slice = b_noise(y[seg * dur * F:(seg + 1) * dur * F], sample_rate=F)
+                        y_slice = b_noise(y[seg * query_length * F:(seg + 1) * query_length * F], sample_rate=F)
                         J = int(np.floor((y_slice.size - F) / H)) + 1
                         xq = [
                             np.expand_dims(extract_mel_spectrogram(signal=y_slice[j * H:j * H + F]), axis=0)
@@ -101,30 +137,38 @@ if __name__ == '__main__':
 
                         # Retrieval
                         tic = time.perf_counter()
-                        D, I = index.search(out.cpu().numpy(), k)
+                        D, I = index.search(out.cpu().numpy(), neighbors)
                         idx, d = query_sequence_search(D, I)
                         start_idx = search_index(idx, sorted_arr)
                         query_times.append(1000 * (time.perf_counter() - tic))
 
                         y_true.append(os.path.basename(audio_file).removesuffix('.wav'))
                         y_pred.append(json_correspondence[str(start_idx)])
-                        offset_true.append(seg * dur)
+                        offset_true.append(seg * query_length)
                         offset_pred.append((idx - start_idx) * H / F)
 
             top_1, res = summary_metrics(np.array(y_true), np.array(y_pred), np.array(offset_true),
                                          np.array(offset_pred))
+            total_times = list(map(lambda x: x[0] + x[1], zip(inference_times, query_times)))
+
+            # Log results
+            logging.info(f"\n\n{7*'*'}Results{7*'*'}\n\n")
             logging.info(res)
-            top_1_hit_rates.append(top_1)
+            logging.info(f"Inference Time: {np.mean(inference_times)}")
+            logging.info(f"Query Time: {np.mean(query_times)} [s]")
+            logging.info(f"Total: {np.mean(total_times)}\n\n")
 
-        total_times = list(map(lambda x: x[0] + x[1], zip(inference_times, query_times)))
+            # Write row
+            model_rows[model_num].append(top_1)
 
-        dur_times[dur] = {
-            'Inference Time': np.mean(inference_times),
-            'Query Time': np.mean(query_times),
-            'Total': np.mean(total_times)
-        }
-        logging.info(dur_times[dur])
+        if len(snrs) % iter:
+            for j, (k, v) in enumerate(model_rows.items(), 1):
+                model_name = models[k]['architecture'] + " (filters)" if models[k][
+                    'filters'] else models[k]['architecture'] + " (no filters)"
 
-    logging.info(f'SNR: {snrs}\nTop 1 hit rate: {top_1_hit_rates}')
-    for dur in durations:
-        logging.info(f'Dur: {dur} | {dur_times[dur]}')
+                if j == len(model_rows):
+                    result_table.add_row([model_name, *v], divider=True)
+                else:
+                    result_table.add_row([model_name, *v])
+
+    logging.info(f"Aggregated Results\n\n{result_table}")
